eeb95523653c13f5aaa90fd9da01e34669d3cb77
nicolasggg <ngaitan55@gmail.com>
2023-01-10 13:44:35 -0500

    Added script to compute ReadALignment quality distributions

---
+++ src/ReadAlignmentTools/ReadAlignmentQualDistribution.java
@@ -0,0 +1,33 @@
+package ReadAlignmentTools;
+
+import ngsep.alignments.ReadAlignment;
+import ngsep.alignments.io.ReadAlignmentFileReader;
+import ngsep.genome.ReferenceGenome;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+public class ReadAlignmentQualDistribution {
+
+    public static final String SEPARATOR = "\t";
+
+    public static void main(String[] args) throws IOException {
+        String alignmentFile = args[0];
+        ReferenceGenome refGenome = new ReferenceGenome(args[1]);
+        try(ReadAlignmentFileReader alignmentReader = new ReadAlignmentFileReader(alignmentFile, refGenome)){
+            int filterFlags = ReadAlignment.FLAG_READ_UNMAPPED;
+            alignmentReader.setFilterFlags(filterFlags);
+            //alignmentReader.setMinMQ(0);
+            Iterator<ReadAlignment> it = alignmentReader.iterator();
+            byte[] qualityDistribution = new byte[257];
+            while(it.hasNext()){
+                ReadAlignment aln = it.next();
+                int quality = aln.getAlignmentQuality();
+                qualityDistribution[quality] += 1;
+            }
+            for(int i = 0; i < 257; i++){
+                System.out.println(i + SEPARATOR + qualityDistribution[i]);
+            }
+        }
+    }
+}
